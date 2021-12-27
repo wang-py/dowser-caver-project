f1ac8cc49e04474de4b6a05bb524abf1d74d4c12
Panyue Wang <pywang@ucdavis.edu>
2021-12-26 18:25:25 -0800

    csv_to_tunnel_points_arrays done

--- caver_to_vina.py
+++ caver_to_vina.py
@@ -11,15 +11,16 @@ import sys
 
 def csv_to_tunnel_points_arrays(tunnel_csv, tunnel_index):
     # skips information before coordinates
-    skip_cols = 10
+    skip_cols = 13
     # only takes coordinates of that tunnel
     tunnel_index_in_csv = (tunnel_index - 1) * 7 + 1
     rows_to_keep = [x for x in range(tunnel_index_in_csv, tunnel_index_in_csv + 3)]
     # scanning through the csv file
-    tunnel_points = pd.read_csv(tunnel_csv, skiprows = lambda x: x not in rows_to_keep)
+    tunnel_points = pd.read_csv(tunnel_csv, skiprows = lambda x: x not in rows_to_keep, \
+        header=None)
     tunnel_points = tunnel_points.to_numpy()
 
-    return tunnel_points[:, skip_cols:]
+    return tunnel_points[:, skip_cols:].astype(float)
 
 def tunnel_points_to_box_configs(tunnel_points, output_folder):
     pass
