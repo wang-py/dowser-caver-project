7e992e3bff773bcafdeda7ede3398cef9e96587c
Panyue Wang <pywang@ucdavis.edu>
2021-12-26 18:15:13 -0800

    reading XYZ

--- caver_to_vina.py
+++ caver_to_vina.py
@@ -9,19 +9,25 @@ import sys
 #a series of config files for AutoDock Vina. Default box size is 6, but it
 #could be customized to each sphere's size.
 
-def csv_to_tunnel_points_arrays(tunnel_csv):
+def csv_to_tunnel_points_arrays(tunnel_csv, tunnel_index):
     # skips information before coordinates
-    skip_cols = 14
-    # 2D array that stores all tunnel points' coordinates
-    tunnel_points = []
-    # temporary array for storing one point
-    point_coords = []
+    skip_cols = 10
+    # only takes coordinates of that tunnel
+    tunnel_index_in_csv = (tunnel_index - 1) * 7 + 1
+    rows_to_keep = [x for x in range(tunnel_index_in_csv, tunnel_index_in_csv + 3)]
+    # scanning through the csv file
+    tunnel_points = pd.read_csv(tunnel_csv, skiprows = lambda x: x not in rows_to_keep)
+    tunnel_points = tunnel_points.to_numpy()
 
-    return tunnel_points
+    return tunnel_points[:, skip_cols:]
 
-def tunnel_points_to_box_configs(tunnel_points):
+def tunnel_points_to_box_configs(tunnel_points, output_folder):
     pass
 
 if __name__ == "__main__":
     tunnel_csv = sys.argv[1]
+    tunnel_index = int(sys.argv[2])
+    output_folder = "boxes"
+    tp = csv_to_tunnel_points_arrays(tunnel_csv, tunnel_index)
+    tunnel_points_to_box_configs(tp, output_folder)
     pass
