95f8d5af76713faaf92923ad3f941ad75ef355ae
Panyue Wang <pywang@ucdavis.edu>
2023-01-30 15:48:57 -0800

    different cutoff

--- plot_hitrate.py
+++ plot_hitrate.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     dowser_waters = sys.argv[2:]
     exp_water_arr = read_exp_water(exp_waters)
     hitrate = []
-    energy_cutoff = np.linspace(0, -12, len(dowser_waters))
+    energy_cutoff = np.linspace(0, -14, len(dowser_waters))
     for one_dowser in dowser_waters:
         dowser_water_arr = dowser_water_pruning(one_dowser)
         hitrate.append(hit_detection(exp_water_arr, dowser_water_arr, 1.4))
