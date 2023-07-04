f54c55a910e7f4d12081879935ed3696481c2ca2
Panyue Wang <pywang@ucdavis.edu>
2023-07-04 16:37:29 -0700

    set residues to reverse

--- reverse_sort_atoms.py
+++ reverse_sort_atoms.py
@@ -12,17 +12,17 @@ def reverse_atom_order(atom_list):
     return rev_list
 
 def extract_residue(resname, atom_info):
-    residue_atoms = [x for x in atom_info if resname in x]
+    residue_atoms = [x for x in atom_info if resname == x.split()[5]]
     return residue_atoms
 
 if __name__ == "__main__":
     atom_info = read_pdb(sys.argv[1])
     output_pdb = sys.argv[2]
-    residues_to_reverse = ["9", "17", "60"]
+    residues_to_reverse = ["62", "47"]
     for res in residues_to_reverse:
         res_atoms = extract_residue(res, atom_info)
         rev_res_atoms = reverse_atom_order(res_atoms)
-        with open(output_pdb, 'w') as output_file:
+        with open(output_pdb + "_" + res + ".pdb", 'w') as output_file:
             output_file.writelines(rev_res_atoms)
     
     pass
