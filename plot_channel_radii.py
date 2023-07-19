b84198c28cc0bdb520fa8d714adedd96968932be
Panyue Wang <pywang@ucdavis.edu>
2023-07-18 20:05:37 -0700

    added auto filename

--- plot_channel_radii.py
+++ plot_channel_radii.py
@@ -1,6 +1,7 @@
 from caver_to_vina import *
 import matplotlib.pyplot as plt
 import sys
+from pathlib import Path
 # Script that reads the csv output of caver and plots tunnel profiles
 
 def plotting(x, y, label):
@@ -23,19 +24,20 @@ def plotting(x, y, label):
     ax.axhline(1.4, color='k', linestyle='--', label="radius of water")
     ax.set_xlabel("Caver channel points", fontsize=label_fontsize)
     ax.set_ylabel("Radius [Å]", fontsize=label_fontsize)
-    ax.set_title("sphere size along caver points", fontsize=label_fontsize)
+    #ax.set_title("sphere size along caver points", fontsize=label_fontsize)
     ax.legend()
 
 if __name__ == "__main__":
     # input tunnel pdb file
     tunnel_pdb = sys.argv[1]
+    input_filename = Path(tunnel_pdb).stem
     # figure title
     #figure_title = sys.argv[2]
     tp = pdb_to_tunnel_points_arrays(tunnel_pdb)
     radii = tp[:, 3]
     site_number = np.arange(tp.shape[0]) + 1
     plotting(site_number, radii, "E channel")
-    plt.savefig("central_axis_radii_paper.png", dpi=150)
+    plt.savefig(input_filename +"_paper.png", dpi=150)
     plt.show()
 
     pass
