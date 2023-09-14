772d2e8d9a80c76315951436c5a48949d24aa365
Panyue Wang <pywang@ucdavis.edu>
2023-09-14 14:36:56 -0700

    fixed

--- remove_duplicate_tunnel_points.py
+++ remove_duplicate_tunnel_points.py
@@ -6,7 +6,7 @@ def remove_duplicate_tp(tp_array):
     """
     removes duplicate tunnel points using numpy.unique() method
     """
-    tp_unique = np.unique(tp_array)
+    tp_unique = np.unique(tp_array, axis=0)
 
     return tp_unique
 
@@ -28,17 +28,20 @@ def write_edited_pdb(tp_array, output_pdb):
     """
     with open(output_pdb, 'w') as output_file:
         for atom_i in range(tp_array.shape[0]):
-            atom_i_pos = tp_array[atom_i, 0:2]
+            atom_i_pos = tp_array[atom_i, 0:3]
             atom_i_pos_pdb = format_atom_position(atom_i_pos)
-            output_file.write("ATOM  {:>5}".format(atom_i) + "  H   FIL T   1     "
+            output_file.write("ATOM  {:>5}".format(atom_i+1) + "  H   FIL T   1    "
                               + atom_i_pos_pdb + "  0.00  " + "%.2f"%tp_array[atom_i, 3]
-                              + "           H")
+                              + "           H\n")
         output_file.write("END")
 
     pass
 
 if __name__ == "__main__":
     tunnel_pdb = sys.argv[1]
+    output_pdb = sys.argv[2]
     tp = pdb_to_tunnel_points_arrays(tunnel_pdb)
     tp_unique = remove_duplicate_tp(tp)
+    write_edited_pdb(tp_unique, output_pdb)
+
     pass
