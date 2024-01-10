9ed94827bf903342a22ddfba18051217011f4793
Panyue Wang <pywang@ucdavis.edu>
2024-01-10 12:51:04 -0800

    read inputs

--- files/refine_results.py
+++ files/refine_results.py
@@ -1,18 +1,24 @@
 import sys
 import os
+import subprocess
 
 if __name__ == "__main__":
     dowser_o_input=sys.argv[1]
     structure_input=sys.argv[2]
-    with open(dowser_o_input) as dowser_o:
-        data = dowser_o.readlines()
-        num_of_water=len(data)
+    with open(dowser_o_input, 'r') as dowser_o:
+        dowser_data = dowser_o.readlines()
+        num_of_water=len(dowser_data)
+    
+    with open(structure_input, 'r') as structure:
+        structure_data = structure.readlines()
 
     print(f"refining the energies of {num_of_water} water molecules...")
 
-    for i in range(num_of_water):
-        current_water=data[i]
+    for i in range(1):
+        current_water=dowser_data[i]
         print(current_water)
+        with open('current_water.pdb', 'w') as cw:
+            cw.write(current_water)
         #CURRENT_WATER_HETATM=$(echo "$CURRENT_WATER" | sed s/ATOM\ \ /HETATM/1 )
         #echo "$CURRENT_WATER_HETATM"
         #sed 's/$CURRENT_WATER/$CURRENT_WATER_HETATM/g' $STRUCTURE_INPUT > current_structure.pdb
