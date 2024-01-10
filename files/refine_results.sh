a0ffec1df08286d0e34693bef3fccc29ab9bd4c9
Panyue Wang <pywang@ucdavis.edu>
2024-01-10 12:21:06 -0800

    replacing ATOM with HETATM

--- files/refine_results.sh
+++ files/refine_results.sh
@@ -2,6 +2,17 @@
 
 DOWSER_O_INPUT=$1
 
+STRUCTURE_INPUT=$2
+
 NUM_OF_WATER=$(awk 'END {print NR}' $DOWSER_O_INPUT)
 
-echo $NUM_OF_WATER
+echo "refining the energies of $NUM_OF_WATER water molecules..."
+
+for ((i=1; i<2; i++))
+    do
+    CURRENT_WATER=$(sed -n ${i}p $DOWSER_O_INPUT)
+    echo "$CURRENT_WATER" > current_water.pdb
+    CURRENT_WATER_HETATM=$(echo "$CURRENT_WATER" | sed s/ATOM\ \ /HETATM/1 )
+    echo "$CURRENT_WATER_HETATM"
+    sed -i s/$CURRENT_WATER/$CURRENT_WATER_HETATM/g $STRUCTURE_INPUT > current_structure.pdb
+done
